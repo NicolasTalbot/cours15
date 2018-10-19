710b82665a0fbd9f04056b8470b25fdb1dead394
B533P-10\533 <talbot.nicolas.nt@gmail.com>
2018-10-19 16:00:49 -0400

    fin du cours

---
+++ exerciceShaders/Assets/cameraeffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class cameraeffect : MonoBehaviour {
+    private Material material;
+	// Use this for initialization
+	void Start () {
+        material = new Material(Shader.Find("Hidden/ShaderImage"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
